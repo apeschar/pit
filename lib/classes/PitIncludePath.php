b4fd6c93161bfa40a12a98aff416943b3999fed1
Albert Peschar <albert@peschar.net>
2011-03-08 20:38:12 +0100

    Complete rename FW -> Pit.

--- lib/classes/PitIncludePath.php
+++ lib/classes/PitIncludePath.php
@@ -1,6 +1,6 @@
 <?php
 
-class FWIncludePath
+class PitIncludePath
 {
   /**
    * Flush include path.
--- load.php
+++ load.php
@@ -3,10 +3,10 @@
 if(!defined('PIT_ROOT')) define('PIT_ROOT', dirname(__FILE__));
 
 // update include path
-require_once PIT_ROOT . '/lib/classes/PitIncludePath.class.php';
+require_once PIT_ROOT . '/lib/classes/PitIncludePath.php';
 PitIncludePath::prepend(PIT_ROOT . '/lib/classes');
 
 // autoloader
-require_once 'PitAutoloader.class.php';
+require_once 'PitAutoloader.php';
 PitAutoloader::register();
 
