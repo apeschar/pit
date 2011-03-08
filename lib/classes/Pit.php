69397d291f0986a18362e2abdbea12fd82edf0e8
Albert Peschar <albert@peschar.net>
2011-03-08 21:34:04 +0100

    Add Pit::completeRequest.

--- lib/classes/Pit.php
+++ lib/classes/Pit.php
@@ -1,9 +1,12 @@
 <?php
 
 class Pit {
+  protected static $_404file;
+
   /**
    * Get a PitRouter object
    *
+   * @return PitRouter
    */
   public static function getRouter() {
     static $router;
@@ -36,6 +39,75 @@ class Pit {
       $router->loadConfFile($config_file);
     } else {
       throw new Exception(sprintf('Configuration file extension should be either .yml or .conf: %s'), $config_file);
+    }
+  }
+
+  /**
+   * Complete the request.
+   *
+   */
+  public static function completeRequest() {
+    // retrieve PATH_INFO
+    $path_info = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '';
+    $path_info = ltrim($path_info, '/');
+    
+    // try to route url
+    $router = self::getRouter();
+    $route = $router->route($path_info);
+
+    if(!$route) {
+      return self::display404();
+    }
+
+    // load controller
+    $controller_class = ucfirst($route['controller']) . 'Controller';
+    $controller = eval('return new ' . $controller_class . ';');
+
+    if(!$controller) {
+      throw new Exception('Could not instantiate controller.');
+    }
+
+    // call action
+    $args = $route;
+    unset($args['controller'], $args['action']);
+    $controller->action($route['action'], $args);
+  }
+
+  /**
+   * Get 404 page location
+   *
+   * @return string
+   */
+  public static function get404File() {
+    if(self::$_404file) {
+      return self::$_404file;
+    } else {
+      return PIT_ROOT . '/data/404.php';
+    }
+  }
+
+  /**
+   * Set 404 page location
+   *
+   * @param string
+   * @return void
+   */
+  public static function set404File($file) {
+    self::$_404file = $file;
+  }
+
+  /**
+   * Display 404 page
+   *
+   * @return void
+   */
+  public static function display404() {
+    header('HTTP/1.0 404 Not Found');
+    $file = self::get404File();
+    if(preg_match('|\.php$|', $file)) {
+      require $file;
+    } else {
+      readfile($file);
     }
   }
 }
