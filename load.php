a70c9fcf8c6f7a4dbce738b7a4e3e5cb7760db46
Albert Peschar <albert@peschar.net>
2011-03-15 20:26:03 +0100

    Add sfYaml.

--- load.php
+++ load.php
@@ -4,6 +4,7 @@ if(!defined('PIT_ROOT')) define('PIT_ROOT', dirname(__FILE__));
 
 // update include path
 require_once PIT_ROOT . '/lib/classes/PitIncludePath.php';
+PitIncludePath::prepend(PIT_ROOT . '/lib/vendor/sfYaml');
 PitIncludePath::prepend(PIT_ROOT . '/lib/classes');
 
 // autoloader
